95da41a79fd4e9119dbc00e9f322c7f027e487e7
David Karn <davidkarnseattle@gmail.com>
2013-12-29 12:29:57 -0800

    Date translation functions wrote

--- date-reader.js
+++ date-reader.js
@@ -92,7 +92,7 @@ function parse_date_format(string, format) {
 	    return full_match; }}
     return false; }
 
-function slots_to_date(parsed_slots) {
+function slots_to_date(parsed_slots) {}
     
 
 function read_date(string, formats) {
@@ -117,7 +117,28 @@ function return_false() {
 function returner(x) { return x; }
 
 function parse_month(string) {
-}
+    var month_table = {jan: 1, feb: 2, mar: 3, apr: 4, may: 5, jun: 6, jul: 7, aug: 8, sep: 9,
+		       oct: 10, nov: 11, dec: 12};
+    return month_table[string.slice(0, 3).toLowerCase()]; }
+
+function parse_year(string) {
+    var year = parseInt(string);
+    if (year < 20) {
+	return 2000 + year; }
+    return 1900 + year; }
+
+function parse_timezone(string) {
+    vat tz_table = {ACDT: '+10:30', ACST: '+09:30', ACT: '+08', ADT: '-03', AEDT: '+11', AEST: '+10', AFT: '+04:30', AKDT: '-08', AKST: '-09', AMST: '-03', AMST: '+05', AMT: '-04', AMT: '+04', ART: '-03', AST: '+03', AST: '-04', AWDT: '+09', AWST: '+08', AZOST: '-01', AZT: '+04', BDT: '+08', BIOT: '+06', BIT: '-12', BOT: '-04', BRT: '-03', BST: '+06', BST: '+01', BTT: '+06', CAT: '+02', CCT: '+06:30', CDT: '-05', CDT: '-04', CEDT: '+02', CEST: '+02', CET: '+01', CHADT: '+13:45', CHAST: '+12:45', CHOT: '+08', ChST: '+10', CHUT: '+10', CIST: '-08', CIT: '+08', CKT: '-10', CLST: '-03', CLT: '-04', COST: '-04', COT: '-05', CST: '-06', CST: '+08', CST: '+09:30', CST: '+10:30', CST: '-05', CT: '+08', CVT: '-01', CWST: '+08:45', CXT: '+07', DAVT: '+07', DDUT: '+10', DFT: '+01', EASST: '-05', EAST: '-06', EAT: '+03', ECT: '-04', ECT: '-05', EDT: '-04', EEDT: '+03', EEST: '+03', EET: '+02', EGST: '+00', EGT: '-01', EIT: '+09', EST: '-05', EST: '+10', FET: '+03', FJT: '+12', FKST: '-03', FKST: '-03', FKT: '-04', FNT: '-02', GALT: '-06', GAMT: '-09', GET: '+04', GFT: '-03', GILT: '+12', GIT: '-09', GMT: '', GST: '-02', GST: '+04', GYT: '-04', HADT: '-09', HAEC: '+02', HAST: '-10', HKT: '+08', HMT: '+05', HOVT: '+07', HST: '-10', ICT: '+07', IDT: '+03', IOT: '+03', IRDT: '+08', IRKT: '+09', IRST: '+03:30', IST: '+05:30', IST: '+01', IST: '+02', JST: '+09', KGT: '+06', KOST: '+11', KRAT: '+07', KST: '+09', LHST: '+10:30', LHST: '+11', LINT: '+14', MAGT: '+12', MART: '-09:30', MAWT: '+05', MDT: '-06', MET: '+01', MEST: '+02', MHT: '+12', MIST: '+11', MIT: '-09:30', MMT: '+06:30', MSK: '+04', MST: '+08', MST: '-07', MST: '+06:30', MUT: '+04', MVT: '+05', MYT: '+08', NCT: '+11', NDT: '-02:30', NFT: '+11:30', NPT: '+05:45', NST: '-03:30', NT: '-03:30', NUT: '-11', NZDT: '+13', NZST: '+12', OMST: '+07', ORAT: '+05', PDT: '-07', PET: '-05', PETT: '+12', PGT: '+10', PHOT: '+13', PHT: '+08', PKT: '+05', PMDT: '-02', PMST: '-03', PONT: '+11', PST: '-08', PYST: '-03', PYT: '-04', RET: '+04', ROTT: '-03', SAKT: '+11', SAMT: '+04', SAST: '+02', SBT: '+11', SCT: '+04', SGT: '+08', SLST: '+05:30', SRT: '-03', SST: '-11', SST: '+08', SYOT: '+03', TAHT: '-10', THA: '+07', TFT: '+05', TJT: '+05', TKT: '+14', TLT: '+09', TMT: '+05', TOT: '+13', TVT: '+12', UCT: '', ULAT: '+08', UTC: '', UYST: '-02', UYT: '-03', UZT: '+05', VET: '-04:30', VLAT: '+10', VOLT: '+04', VOST: '+06', VUT: '+11', WAKT: '+12', WAST: '+02', WAT: '+01', WEDT: '+01', WEST: '+01', WET: '', WST: '+08', YAKT: '+10', YEKT: '+06', Z: ''};
+    return parse_gmt(tz_table[string.toUpperCase()]); }
+
+function parse_gmt(string) {
+    var matches = string.match(/([-+])?([0-9]{2,2})?(:[0-9]{2,2})?/);
+    var minutes = matches[3] || 0;
+    var hours = matches[2] || 1;
+    var sign = matches[1] || '+';
+    var seconds =  (minutes * 60) + (hours * 60 * 60);
+    if (sign === '+') { return seconds; }
+    return 0 - seconds; }
 
 var date_translation_table = {
     'day-leading-zero': {slot: 'day', parser: parseInt},
@@ -143,7 +164,31 @@ var date_translation_table = {
     'gmt-difference-colon': {slot: 'timezone', parser: parse_gmt},
     'timezone-offset-seconds': {slot: 'timezone', parser: parse_tz_offset_seconds},
     'unix-time': {slot: 'unix', parser: parse_unix},
-    'unix-time-full': {slot: 'unix', parser: parse_unix}};
+    'unix-time-full': {slot: 'unix', parser: parse_unix}};var date_translation_table = {
+    'day-leading-zero': {slot: 'day', parser: parseInt},
+    'day': {slot: 'day', parser: parseInt},
+    'day-of-year': {slot: 'day_of_year', parser: parseInt},
+    'month-full': {slot: 'month', parser: parse_month},
+    'month-leading-zero': {slot: 'month', parser: parseInt},
+    'month-three-letters': {slot: 'month', parser: parse_month},
+    'month': {slot: 'month', parser: parseInt},
+    'iso-8601-year': {slot: 'year', parser: parseInt},
+    'year': {slot: 'year', parser: parseInt},
+    'year-two-digit': {slot: 'year', parser: parse_year},
+    'am-pm': {slot: 'am_pm', parser: returner},
+    'hour': {slot: 'hour', parser: parseInt},
+    '24-hour': {slot: 'hour', parser: parseInt},
+    'hour-leading-zero': {slot: 'hour', parser: parseInt},
+    '24-hour-leading-zero': {slot: 'hour', parser: parseInt},
+    'minute-leading-zero': {slot: 'minute', parser: parseInt},
+    'second-leading-zero': {slot: 'second', parser: parseInt},
+    'tenth-second': {slot: 'tenth_second', parser: parseInt},
+    'timezone-abr': {slot: 'timezone', parser: parse_timezone},
+    'gmt-difference': {slot: 'timezone', parser: parse_gmt},
+    'gmt-difference-colon': {slot: 'timezone', parser: parse_gmt},
+    'timezone-offset-seconds': {slot: 'timezone', parser: parseInt},
+    'unix-time': {slot: 'unix', parser: parseInt},
+    'unix-time-full': {slot: 'unix', parser: parseInt}};
 
 ((nanosecond
      (tenth-second ,(lambda (x) (* x 100000))))
